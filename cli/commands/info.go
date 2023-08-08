31343f57d8bd7065e9b0fcc71906ac0fd4be8507
Rob Gonnella <robgonnella@gmail.com>
2023-08-08 10:28:25 -0400

    Minor updates
    - Updates READMEs
    - Updates info command

--- cli/commands/info.go
+++ cli/commands/info.go
@@ -13,17 +13,13 @@ func info() *cobra.Command {
 		Use:   "info",
 		Short: "Print detailed app info",
 		Run: func(cmd *cobra.Command, args []string) {
-			nmapCmd := exec.Command("nmap", "--version")
-			nmapInfo, _ := nmapCmd.Output()
-
 			ansibleCmd := exec.Command("ansible", "--version")
 			ansibleInfo, _ := ansibleCmd.Output()
 
 			fmt.Printf(
-				"%s: %s\n\n%s\n%s\n",
+				"%s: %s\n\n%s\n",
 				app_info.NAME,
 				app_info.VERSION,
-				nmapInfo,
 				ansibleInfo,
 			)
 		},
